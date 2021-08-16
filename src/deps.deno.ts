d21c9cc1a517772f02fc5988751e3117de9bc47a
KnorpelSenf <shtrog@gmail.com>
2021-08-16 13:05:20 +0200

    Fix types

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -8,4 +8,4 @@ export type {
     RawApi,
     ApiCallFn,
 } from 'https://deno.land/x/grammy@v1.3.0/mod.ts'
-export * from 'https://cdn.skypack.dev/@grammyjs/types@v2.2.5?dts'
+export * from 'https://cdn.skypack.dev/@grammyjs/types@v2.2.6?dts'
